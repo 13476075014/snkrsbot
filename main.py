af1c4bd10d9200dcb00cc820941eea8e2a7fec9b
lktop <1277843354@qq.com>
2018-11-11 16:26:28 +0800

    2018 11 11

--- main.py
+++ main.py
@@ -1,11 +1,68 @@
 #coding=utf-8
-from a1 import *
-from a2 import *
 from login import *
+import json
+import re
+import requests
 from WebLogin import *
+import threading,sqlite3,time
+from multiprocessing import Process,Queue
+
+
+def getPostpayLink(productId, shoesLink):
+    url = "https://api.nike.com/merch/products/v2/" + productId
+    response = requests.get(url).json()
+    postpayLink = shoesLink + "?LEStyleColor={styleColor}&LEPaymentType=Alipay".format(
+        styleColor=response["styleColor"])
+    return postpayLink
+
+
+def buySet(shoesUrl):
+    response = requests.get(shoesUrl).text
+
+    launchId = re.findall(r"\"launchViewId\":\"(.*?)\"", response, re.S | re.I)[0]
+    size = re.findall(r"\"sizes\":(.*?),\"_fetchedAt\"", response, re.S | re.I)[0]
+    productId = re.findall(r"product\":{\"productId\":\"(.*?)\"", response, re.S | re.I)[0]
+
+    shoesLink = re.findall(r"canonical\" href=\"(.*?)\"", response, re.S | re.I)[0]
+    postpayLink = getPostpayLink(productId, shoesLink)
+
+    stock = []
+    outOfStock = []
+
+    j_size = json.loads(size)
+
+    for s in j_size:
+        if j_size[s]["available"]:
+            stock.append(j_size[s]["localizedSize"])
+        else:
+            outOfStock.append(j_size[s]["localizedSize"])
+    stock.sort()
+    outOfStock.sort()
+
+    if len(outOfStock) > 0:
+        print(" ".join(outOfStock) + "尺码缺货\n请选择" + " ".join(stock))
+    else:
+        print("所有尺码有货\n请选择" + " ".join(stock))
+
+    shoesSize = input("-->")
+
+    for s in j_size:
+        if j_size[s]["localizedSize"] == shoesSize:
+            usShoesSize = s
+            skuId = j_size[s]["skuId"]
+
+    # print("请设置好默认收货地址")
+
+    # print("请选择支付方式 1.银联 2.微信 3.支付宝")
+    # purMethod = input("-->")
+
+    # shoesBuyUrl = shoesUrl + "/?productId=" + launchId + "&size=" + usShoesSize
+
+    return shoesSize, launchId, skuId, productId, postpayLink
+
 if __name__ == '__main__':
     shoesSize, launchId, skuId, productId, postpayLink = buySet(
-        "https://www.nike.com/cn/launch/t/air-revaderchi-gym-red-mink-brown/")
+        "https://www.nike.com/cn/launch/t/air-max-270-volt-black-oil-grey/")
 
     threads = int(input("根据参与用户数量自行设置一个购买和监控的线程数:"))
 
